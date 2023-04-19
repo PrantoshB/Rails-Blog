14d3882f9d55c20b922e9f91ba708aadb48ba268
Prantosh Biswas <pbiswas1007@gmail.com>
2023-04-19 21:29:11 +0530

    Remove unnecessary line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :confirmable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'users_id'
